e297526466fb4f1ff8324b3dbbe5ea914d3978cf
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-03-10 10:46:28 +0530

    Add MongoDB connection and event model schema

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const connectDB = require('./config/database');
 
 const app = express();
 const port = 3000;
 
+// Connect to the database
+connectDB();
+
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.use('/api/events', require('./Routes/events'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
